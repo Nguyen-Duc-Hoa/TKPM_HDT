a8510d9cc72074c847ae719f579741f7bf4c49a9
Nguyen-Duc-Hoa <vic.nguyenduchoa@gmail.com>
2021-04-18 21:46:12 +0700

    add mainpage controller

---
+++ Online_Academy/Areas/Student/Controllers/MainPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Academy.Areas.Student.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: Student/MainPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
