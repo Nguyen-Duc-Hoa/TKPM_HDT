5c5ccd5a363e18380fa9479ba16a61e1c64aad37
yuhlee610 <yuhlee610@gmail.com>
2021-05-01 15:54:29 +0700

    view category

---
+++ Online_Academy/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Online_Academy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Academy.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private DB_A72902_TKPMEntities db = new DB_A72902_TKPMEntities();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            // API chưa lấy subcategory khi lấy category
+            List<Category> lstCategories = db.Categories.ToList();
+            ViewBag.lstCategories = lstCategories;
+            
+            return View();
+        }
+    }
+}
