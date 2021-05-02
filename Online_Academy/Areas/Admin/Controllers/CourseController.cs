627b0f7ef7a58fa058db9d519645696704d8a90d
yuhlee610 <yuhlee610@gmail.com>
2021-05-02 15:21:23 +0700

    manage courses

--- Online_Academy/Areas/Admin/Controllers/CourseController.cs
+++ Online_Academy/Areas/Admin/Controllers/CourseController.cs
@@ -11,13 +11,70 @@ namespace Online_Academy.Areas.Admin.Controllers
     {
         private DB_A72902_TKPMEntities db = new DB_A72902_TKPMEntities();
         // GET: Admin/Course
-        public ActionResult Index()
+        public int pageSize = 1;
+        public ActionResult Index(string txtSearch, int? page)
         {
             CourseClient client = new CourseClient();
             // Modify API to get course available
-            List<Course> lstCourses = db.Courses.Where(c => c.state == true).ToList();
-            ViewBag.lstCourses = lstCourses;
+            List<Course> lstCourses = db.Courses.ToList();
+
+            if (!String.IsNullOrEmpty(txtSearch))
+            {
+                ViewBag.txtSearch = txtSearch;
+                lstCourses = lstCourses.Where(c => c.name.Contains(txtSearch)).ToList();
+            }
+
+            if (page > 0)
+            {
+                page = page;
+            }
+            else
+            {
+                page = 1;
+            }
+
+            int start = (int)(page - 1) * pageSize;
+
+            ViewBag.pageCurrent = page;
+            int totalPage = lstCourses.Count();
+            // tổng số trang cần hiển thị
+            float totalNumsize = (totalPage / (float)pageSize);
+            int numSize = (int)Math.Ceiling(totalNumsize);
+            ViewBag.numSize = numSize;
+            ViewBag.courses = lstCourses.OrderByDescending(x => x.id).Skip(start).Take(pageSize);
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult Approve(int idCourse)
+        {
+            try
+            {
+                var dbCourse = db.Courses.Where(c => c.id == idCourse).FirstOrDefault();
+                dbCourse.state = true;
+                db.SaveChanges();
+                return Content("Success");
+            }
+            catch
+            {
+                return Content("Fail");
+            }
+        }
+        [HttpGet]
+        public ActionResult Cancel(int idCourse)
+        {
+            // Xóa luôn cái course
+            try
+            {
+                var dbCourse = db.Courses.Where(c => c.id == idCourse).FirstOrDefault();
+                db.Courses.Remove(dbCourse);
+                db.SaveChanges();
+                return Content("Success");
+            }
+            catch
+            {
+                return Content("Fail");
+            }
         }
     }
 }
