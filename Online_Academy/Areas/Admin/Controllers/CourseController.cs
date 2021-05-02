427d1d2f9ffce49b72017e32482fc6461fb653da
yuhlee610 <yuhlee610@gmail.com>
2021-05-02 13:20:05 +0700

    add view manage courses

---
+++ Online_Academy/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using Online_Academy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Academy.Areas.Admin.Controllers
+{
+    public class CourseController : Controller
+    {
+        private DB_A72902_TKPMEntities db = new DB_A72902_TKPMEntities();
+        // GET: Admin/Course
+        public ActionResult Index()
+        {
+            CourseClient client = new CourseClient();
+            // Modify API to get course available
+            List<Course> lstCourses = db.Courses.Where(c => c.state == true).ToList();
+            ViewBag.lstCourses = lstCourses;
+            return View();
+        }
+    }
+}
