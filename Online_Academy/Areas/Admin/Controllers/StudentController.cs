cb65159d0c8b95d61c205e7982172dfa8adc86b1
yuhlee610 <yuhlee610@gmail.com>
2021-05-02 19:11:38 +0700

    manage user

--- Online_Academy/Areas/Admin/Controllers/StudentController.cs
+++ Online_Academy/Areas/Admin/Controllers/StudentController.cs
@@ -7,21 +7,20 @@ using System.Web.Mvc;
 
 namespace Online_Academy.Areas.Admin.Controllers
 {
-    public class CourseController : Controller
+    public class StudentController : Controller
     {
         private DB_A72902_TKPMEntities db = new DB_A72902_TKPMEntities();
-        // GET: Admin/Course
-        public int pageSize = 1;
+        public int pageSize = 3;
+        // GET: Admin/Student
         public ActionResult Index(string txtSearch, int? page)
         {
-            CourseClient client = new CourseClient();
-            // Modify API to get course available
-            List<Course> lstCourses = db.Courses.ToList();
+            UsersClient client = new UsersClient();
+            var data = client.findAll();
 
             if (!String.IsNullOrEmpty(txtSearch))
             {
                 ViewBag.txtSearch = txtSearch;
-                lstCourses = lstCourses.Where(c => c.name.Contains(txtSearch)).ToList();
+                data = data.Where(x => x.name.Contains(txtSearch));
             }
 
             if (page > 0)
@@ -34,24 +33,22 @@ namespace Online_Academy.Areas.Admin.Controllers
             }
 
             int start = (int)(page - 1) * pageSize;
-
             ViewBag.pageCurrent = page;
-            int totalPage = lstCourses.Count();
+            int totalPage = data.Count();
             // tổng số trang cần hiển thị
             float totalNumsize = (totalPage / (float)pageSize);
             int numSize = (int)Math.Ceiling(totalNumsize);
             ViewBag.numSize = numSize;
-            ViewBag.courses = lstCourses.OrderByDescending(x => x.id).Skip(start).Take(pageSize);
-
+            ViewBag.users = data.OrderByDescending(x => x.id).Skip(start).Take(pageSize);
             return View();
         }
         [HttpPost]
-        public ActionResult Approve(int idCourse)
+        public ActionResult Approve(int idStudent)
         {
             try
             {
-                var dbCourse = db.Courses.Where(c => c.id == idCourse).FirstOrDefault();
-                dbCourse.state = true;
+                var dbStudent = db.Users.Where(c => c.id == idStudent).FirstOrDefault();
+                dbStudent.state = true;
                 db.SaveChanges();
                 return Content("Success");
             }
@@ -61,13 +58,12 @@ namespace Online_Academy.Areas.Admin.Controllers
             }
         }
         [HttpPost]
-        public ActionResult Cancel(int idCourse)
+        public ActionResult Cancel(int idStudent)
         {
-            // Xóa luôn cái course
             try
             {
-                var dbCourse = db.Courses.Where(c => c.id == idCourse).FirstOrDefault();
-                dbCourse.state = false;
+                var dbStudent = db.Users.Where(c => c.id == idStudent).FirstOrDefault();
+                dbStudent.state = false;
                 db.SaveChanges();
                 return Content("Success");
             }
